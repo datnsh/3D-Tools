fbd1a6295230e4c6d9ef05abdea3c4d566d21f87
datnsh <datnsh@gmail.com>
2026-01-30 14:44:48 +0700

    Update

--- Validation_Tool/main.py
+++ Validation_Tool/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import qtmax, importlib
 from controller import ValidationController
 from model import ValidationVariables
@@ -10,15 +11,28 @@ from controller.ValidationController import ValidationController
 from model.ValidationUtils import ValidationUtils
 from view.ValidationUI import ValidationUI
 
+=======
+import qtmax, importlib, sys, os
+from pymxs import runtime as rt
+path = os.path.dirname(__file__)
+if path not in sys.path:
+    sys.path.append(path)
+from validation_tool.model import validation_utils as utils
+from validation_tool.model import validation_variables as var
+from validation_tool.view import validation_ui as ui
+from validation_tool.controller import validation_controller as control
+mods = [utils, var, ui, control]
+for mod in mods:
+    importlib.reload(mod)
+>>>>>>> b65d765 (Update)
 def main():
     mainWindow = qtmax.GetQMaxMainWindow()
-    ui = ValidationUI(parent=mainWindow)
-    util = ValidationUtils()
-    controller = ValidationController(ui,util)
-    ui.setObjectName(ValidationVariables.TOOL_TITLE)
-    ui.closeLastInstance(mainWindow, ValidationVariables.TOOL_TITLE)
-    ui.setFloating(True)
-    ui.show()
-
+    main_ui = ui.ValidationUI(parent=mainWindow)
+    util = utils.ValidationUtils()
+    controller = control.ValidationController(main_ui,util)
+    main_ui.setObjectName(var.TOOL_TITLE)
+    main_ui.close_last_instance(mainWindow, var.TOOL_TITLE)
+    main_ui.setFloating(True)
+    main_ui.show()
 if __name__ == "__main__":
     main()
