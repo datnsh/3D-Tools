4bc1831fea6d4d26b686d1e0cc13dd65bed03fa5
datnsh <datnsh@gmail.com>
2026-01-30 14:44:48 +0700

    Update

--- polycount_checker/main.py
+++ polycount_checker/main.py
@@ -1,12 +1,16 @@
 import sys, os, importlib
+from pymxs import runtime as rt
 path = os.path.dirname(__file__)
 if path not in sys.path:
     sys.path.append(path)
-import controller.poly_checker_controller as poly_checker_controller
-importlib.reload(poly_checker_controller)
-from controller.poly_checker_controller import PolyController
+from polycount_checker.controller import poly_checker_controller as poly_checker_controller
+from polycount_checker.model import poly_checker_utils as poly_checker_utils
+from polycount_checker.model import poly_checker_variables as poly_checker_variables
+modules = [poly_checker_utils, poly_checker_controller, poly_checker_variables]
+for mod in modules:
+    importlib.reload(mod)
 
 def main():
-    controller = PolyController()
+    controller = poly_checker_controller.PolyController()
 if __name__ == "__main__":
     main()
